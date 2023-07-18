9e90c3bd4efba5a8c5dcfe8af30178e27d3a4324
nce27 <nc3elliott@gmail.com>
2023-07-18 14:02:31 +0100

    Passing tests for withdraw and displaying balance

--- account.js
+++ account.js
@@ -4,10 +4,30 @@ class Account {
     this.balance = 0;
   }
 
+  print(amount) {
+    this.balance
+  }
+    
   deposit(amount) {
     this.balance += amount;
   }
 
+  withdraw(amount) {
+    
+    this.balance -= amount;
+  }
+
+//   calculateBalance() {
+//     if (this.transactions.length === 0) {
+//       return 0;
+//     }
+//     return this.transactions[this.transactions.length - 1].balance; //looks at the most recent balance log
+//   }
+// }
+
 }
 
 module.exports = Account;
+
+
+
--- account.spec.js
+++ account.spec.js
@@ -8,4 +8,47 @@ describe('Account', () => {
     expect(account.balance).toBe(10);
   });
 
+  it ('withdraws the a chosen amount and displays the total balance', () =>{
+    const account = new Account();
+    account.withdraw(10);
+    expect(account.balance).toBe(-10);
+  });
+
+  it ('prints the total balance', () =>{
+    const account = new Account();
+    account.print(10);
+    expect(account.balance).toBe(0);
+  });
 });
+
+
+
+// const BankAccount = require('./BankAccount');
+
+// describe('BankAccount', () => {
+//   let account;
+
+//   beforeEach(() => {
+//     account = new BankAccount();
+//   });
+
+//   test('should correctly deposit an amount and update balance', () => {
+//     account.deposit(1000, '10-01-2023');
+//     expect(account.calculateBalance()).toBe(1000);
+
+//     account.deposit(2000, '13-01-2023');
+//     expect(account.calculateBalance()).toBe(3000);
+//   });
+
+//   test('should correctly withdraw an amount and update balance', () => {
+//     account.deposit(3000, '13-01-2023');
+//     account.withdraw(500, '14-01-2023');
+//     expect(account.calculateBalance()).toBe(2500);
+//   });
+
+//   test('should print bank statement in the correct format', () => {
+//     account.deposit(1000, '10-01-2023');
+//     account.deposit(2000, '13-01-2023');
+//     account.withdraw(500, '14-01-2023');
+//   }
+// )});
